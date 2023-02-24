958aff876f06f762aceb02f5dd3b774684a9c5c2
narumifumiya <ec2-user@ip-172-31-42-249.ap-northeast-1.compute.internal>
2023-02-24 06:04:51 +0000

    [users controller,view追加]

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
+  
   devise_for :users
   root to: 'homes#top'
   get 'home/about' => 'homes#about', as: 'about'
   
   resources :books, only: [:index, :show, :edit]
+  resources :users, only: [:index, :show, :edit]
   
 end
